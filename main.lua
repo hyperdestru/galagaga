358cffdad2d74812b4a8af38ea6ca7474436fd81
hyperdestru <slotuser@gmail.com>
2019-03-28 19:40:45 +0100

    Working mix between my modular way and more procedural way of gc (w/ CreateSprite func)

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ function love.load()
     game_height = love.graphics.getHeight()
 
     ship = {}
-    ship.image = love.graphics.newImage('images/heros.png')
+    ship.image = love.graphics.newImage('images/ship.png')
     ship.width = ship.image:getWidth()
     ship.height = ship.image:getHeight()
     ship.x = game_width / 2
@@ -23,6 +23,23 @@ function love.load()
     laser.width = laser.image:getWidth()
     laser.height = laser.image:getHeight()
     laser.sound = love.audio.newSource('sounds/shoot.wav', 'static')
+
+    Sprites = {}
+    Tirs = {}
+end
+
+function CreateSprite(pName, px, py)
+    sprite = {}
+    sprite.img = love.graphics.newImage('images/' .. pName .. '.png')
+    sprite.x = px
+    sprite.y = py
+    sprite.w = sprite.img:getWidth()
+    sprite.h = sprite.img:getHeight()
+    sprite.kill = false
+
+    table.insert(Sprites, sprite)
+
+    return sprite
 end
 
 function love.update(dt)
@@ -47,20 +64,39 @@ function love.update(dt)
     end
     ----
 
-    laser.x = ship.x
-    laser.y = ship.y - ship.width
-
-    if love.keyboard.isDown('space') then
-        ship.fire = true
-    else
-        ship.fire = false
+    local n
+    for n = #Tirs, 1, -1 do
+        local tir = Tirs[n]
+        tir.y = tir.y + tir.v
+        if tir.y < 0 or tir.y > game_height then
+            tir.kill = true
+            table.remove(Tirs, n)
+        end
     end
+
+    for n = #Sprites, 1, -1 do
+        if Sprites[n].kill == true then
+            table.remove(Sprites, n)
+        end
+    end
+    
 end
 
 function love.draw()
     love.graphics.draw(ship.image, ship.x, ship.y, 0, 2, 2, ship.width / 2, ship.height / 2)
 
-    if ship.fire == true then
-        love.graphics.draw(laser.image, laser.x, laser.y, 0, 2, 2, laser.width / 2, laser.height / 2)
+    local n
+    for n = 1, #Sprites do
+        local s = Sprites[n]
+        love.graphics.draw(s.img, s.x, s.y, 0, 2, 2, s.w / 2, s.h / 2)
+    end
+end
+
+function love.keypressed(key)
+    if key == 'space' then
+        local tir = CreateSprite('laser1', ship.x, ship.y - ship.width)
+        tir.v = -laser.speed
+        table.insert(Tirs, tir)
+        laser.sound:play()
     end
 end
