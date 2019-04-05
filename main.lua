ff967cab9f2be7a9aaed85b0a8c1186fd187799c
hyperdestru <slotuser@gmail.com>
2019-04-05 13:53:47 +0200

    Big cleaning, no more using equations with dts and speeds : all in parameters, continued aliens func with random dir

--- main.lua
+++ main.lua
@@ -1,3 +1,15 @@
+io.stdout:setvbuf('no')
+love.graphics.setDefaultFilter("nearest")
+
+sprites = {}
+
+ship = {}
+
+lasers = {}
+lasers.sound = nil
+
+aliens = {}
+
 function CreateSprite(pName, px, py, pvx, pvy)
     sprite = {}
     sprite.img = love.graphics.newImage('images/' .. pName .. '.png')
@@ -14,68 +26,103 @@ function CreateSprite(pName, px, py, pvx, pvy)
     return sprite
 end
 
-function CreateAlien(pType, px, py)
+function CreateAlien(pType, px, py, pvx, pvy)
     local name = 'enemy' .. pType
-    local alien = CreateSprite(name, px, py)
+    local alien = CreateSprite(name, px, py, pvx, pvy)
+
+    if pType == '1' then
+
+        alien.vx = 0
+        alien.vy = 2
+
+    elseif pType == '2' then
+
+        local direction = love.math.random(1, 2)
+
+        if direction == 1 then
+            alien.vx = 1
+        elseif direction == 2 then
+            alien.vx = -1
+        end
+
+        alien.vy = 2
+    end
+
     table.insert(aliens, alien)
+
+    return alien
 end
 
+function StartGame()
+    ship.x = game_width / 2
+    ship.y = game_height - ship.height * 2
+    CreateAlien('1', game_width / 2 - 200, game_height / 2 - 100, 0, 0)
+    CreateAlien('2', game_width / 2, game_height / 2 - 100, 0, 0)
+end
+
+
 function love.load()
+
     game_width = love.graphics.getWidth()
     game_height = love.graphics.getHeight()
 
-    --sprites will contain every sprites of the game (hero, enemies, lasers etc) : we'll only have one sprites loop in the draw()
-    sprites = {}
+    ship = CreateSprite('ship', game_width / 2, game_height / 2, 5, 5)
 
-    ----OUR HERO
-    ship = {}
-    ship = CreateSprite('ship', game_width / 2, game_height / 2, 0, 0)
-    ship.y = game_height - ship.height * 2
-    ship.speed = 5
-    ----
-
-    --Will contain every lasers shoot
-    lasers = {}
-    lasers.speed = 10
     lasers.sound = love.audio.newSource('sounds/shoot.wav', 'static')
 
-    aliens = {}
-    CreateAlien('1', game_width / 2, game_height / 2)
+    StartGame()
 
 end
 
 function love.update(dt)
-    ship.vx = ship.speed + (60 * dt)
-    ship.vy = ship.speed + (60 * dt)
 
-    ----SHIP CONTROL
-    if love.keyboard.isDown('right') then
-        ship.x = ship.x + ship.vx
-    end
+    ----HERO SHIP
+    do
+        if love.keyboard.isDown('right') then
+            ship.x = ship.x + ship.vx
+        end
 
-    if love.keyboard.isDown('down') then
-        ship.y = ship.y + ship.vy
-    end
+        if love.keyboard.isDown('down') then
+            ship.y = ship.y + ship.vy
+        end
 
-    if love.keyboard.isDown('left') then
-        ship.x = ship.x - ship.vx
-    end
+        if love.keyboard.isDown('left') then
+            ship.x = ship.x - ship.vx
+        end
 
-    if love.keyboard.isDown('up') then
-        ship.y = ship.y - ship.vy
+        if love.keyboard.isDown('up') then
+            ship.y = ship.y - ship.vy
+        end
     end
     ----
 
     ----LASERS
-    lasers.vy = lasers.speed + (60 * dt)
+    do
+        local n
+        for n = #lasers, 1, -1 do
+            local laser = lasers[n]
+            laser.y = laser.y + laser.vy
 
-    local n
-    for n = #lasers, 1, -1 do
-        local laser = lasers[n]
-        laser.y = laser.y - lasers.vy
-        if laser.y < 0 or laser.y > game_height then
-            laser.kill = true
-            table.remove(lasers, n)
+            if laser.y < 0 or laser.y > game_height then
+                laser.kill = true
+                table.remove(lasers, n)
+            end
+        end
+    end
+    ----
+
+    ----ALIENS
+    do
+        local n
+        for n = #aliens, 1, -1 do
+            local alien = aliens[n]
+            alien.x = alien.x + alien.vx
+            alien.y = alien.y + alien.vy
+
+            if alien.y > game_height then
+                alien.kill = true
+                table.remove(aliens, n)
+            end
         end
     end
     ----
@@ -87,20 +134,25 @@ function love.update(dt)
         end
     end
     ----
+
 end
 
 function love.draw()
+
     local n
     for n = 1, #sprites do
         local s = sprites[n]
         love.graphics.draw(s.img, s.x, s.y, 0, 2, 2, s.width / 2, s.height / 2)
     end
+
 end
 
 function love.keypressed(key)
+
     if key == 'space' then
-        local laser = CreateSprite('laser1', ship.x, ship.y - ship.width, 0, 0)
+        local laser = CreateSprite('laser1', ship.x, ship.y - ship.width, 0, -10)
         table.insert(lasers, laser)
         lasers.sound:play()
     end
+
 end
